e11e56c46d0527b92323a26148e50a056e2b58c8
ankitjain28may <ankitjain28may77@gmail.com>
2017-07-16 20:22:39 +0530

    Added

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,82 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-// Wait for Cordova to load
-//
+
+// Create Database
+var dbobj;
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// Cordova is ready
-//
-
-
-
 function onDeviceReady() {
-    console.log("Device Ready");
-    var db = window.openDatabase("shortName", "1.5", "displayName", 65536)
-    db.transaction(populateDB, errorCB, successCB);
+  console.log("Device Ready");
+ if(device.platform=="Android"){
+            dbobj = window.sqlitePlugin.openDatabase({name: "Database"});
+ }
+ else{
+    dbobj = window.openDatabase("Database", "1.0", "Database", 65536);
+  //dbobj = window.openDatabase("databasename", "4", "Cordova Demo",'');
+ }
+  dbobj.transaction(createSchema, errorInSchema, successInSchema);
 }
 
-function populateDB(tx) {
-     tx.executeSql('DROP TABLE IF EXISTS DEMO');
-     tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+
+// Create Tables
+function createSchema(tx)
+{
+  tx.executeSql('DROP TABLE IF EXISTS name_list');
+  tx.executeSql('CREATE TABLE IF NOT EXISTS name_list(nID INTEGER PRIMARY KEY AUTOINCREMENT,sName TEXT)');
+  tx.executeSql('INSERT INTO name_list (sName) VALUES ("First row")');
+  tx.executeSql('INSERT INTO name_list (sName) VALUES ("Second row")');
 }
 
-function errorCB(err) {
-    alert("Error processing SQL: "+err.code);
+function errorInSchema()
+{
+    alert("Error to create schema");
 }
 
-function successCB() {
-    alert("success!");
+function successInSchema()
+{
+    alert("Schema creation successful");
 }
 
-function queryDB(tx) {
-    $("#name").val("working");
-    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
-}
-
-    // Query the success callback
-    //
-function querySuccess(tx, results) {
-  console.log("Returned rows = " + results.rows.length);
-  // this will be true since it was a select statement and so rowsAffected was 0
-  if (!results.rowsAffected) {
-    console.log('No rows affected!');
-    return false;
-  }
-  // for an insert statement, this property will return the ID of the last inserted row
-  console.log("Last inserted row ID = " + results.insertId);
-    }
-
-    // Transaction error callback
-    //
-    function errorCB(err) {
-        console.log("Error processing SQL: "+err.code);
-    }
-
-    // Transaction success callback
-    //
-
-
-$(".submit").click(function() {
-    var name = $("#name").val();
-        console.log("DB created");
-        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-        db.transaction(queryDB, errorCB);
+// Insert Records
+$("#submit").click(function() {
+  dbobj.transaction(insertRecord, errorDB, successDB);
+  dbobj.transaction(selectRecords, errorDB, successDB);
 })
+
+function insertRecord(tx)
+{
+    tx.executeSql('INSERT INTO name_list (sName) VALUES("Bhumi Shah")',[],SuccessInsert,errorInsert);
+}
+
+function SuccessInsert(tx,result){
+          alert("Last inserted ID = " + result.insertId);
+}
+
+function errorInsert(error){
+    alert("Error processing SQL: "+error.code);
+}
+
+// Fetch
+
+function selectRecords(tx)
+{
+    tx.executeSql('SELECT * FROM name_list', [], successResults, errorInQuery);
+}
+
+function successResults(tx,results){
+  //    alert(JSON.stringify(results));
+  var sData = jquery.ParseJSON(results);
+  console.log(sData);
+  var nLength = results.rows.length;
+  console.log(nLength);
+  // for(var c=0;c<=nLength;c++){
+    // execute or place your desired statement
+  // }
+}
+
+function errorInQuery(error){
+    alert("Error processing SQL: "+error.code);
+}
