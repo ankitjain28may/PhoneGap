2a7d1caf2aba385ffff218361cc5b815a0d942ea
ankitjain28may <ankitjain28may77@gmail.com>
2017-07-16 18:13:48 +0530

    Testing

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -394,28 +394,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
-        "id": "cordova-plugin-device-motion.Acceleration",
-        "pluginId": "cordova-plugin-device-motion",
-        "clobbers": [
-            "Acceleration"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
-        "id": "cordova-plugin-device-motion.accelerometer",
-        "pluginId": "cordova-plugin-device-motion",
-        "clobbers": [
-            "navigator.accelerometer"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device-motion/src/browser/AccelerometerProxy.js",
-        "id": "cordova-plugin-device-motion.AccelerometerProxy",
-        "pluginId": "cordova-plugin-device-motion",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
         "id": "cordova-plugin-device-orientation.CompassError",
         "pluginId": "cordova-plugin-device-orientation",
@@ -444,80 +422,6 @@ module.exports = [
         "id": "cordova-plugin-device-orientation.CompassProxy",
         "pluginId": "cordova-plugin-device-orientation",
         "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-media/www/MediaError.js",
-        "id": "cordova-plugin-media.MediaError",
-        "pluginId": "cordova-plugin-media",
-        "clobbers": [
-            "window.MediaError"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-media/www/Media.js",
-        "id": "cordova-plugin-media.Media",
-        "pluginId": "cordova-plugin-media",
-        "clobbers": [
-            "window.Media"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-media/www/browser/Media.js",
-        "id": "cordova-plugin-media.BrowserMedia",
-        "pluginId": "cordova-plugin-media",
-        "clobbers": [
-            "window.Media"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/src/browser/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar.Browser",
-        "pluginId": "cordova-plugin-statusbar",
-        "merges": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/src/browser/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar.Browser",
-        "pluginId": "cordova-plugin-statusbar",
-        "merges": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
-        "id": "cordova-plugin-vibration.notification",
-        "pluginId": "cordova-plugin-vibration",
-        "merges": [
-            "navigator.notification",
-            "navigator"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-vibration/src/browser/Vibration.js",
-        "id": "cordova-plugin-vibration.Vibration",
-        "pluginId": "cordova-plugin-vibration",
-        "merges": [
-            "navigator.notification",
-            "navigator"
-        ]
     },
     {
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
@@ -605,6 +509,30 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/cordova-plugin-media/www/MediaError.js",
+        "id": "cordova-plugin-media.MediaError",
+        "pluginId": "cordova-plugin-media",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/Media.js",
+        "id": "cordova-plugin-media.Media",
+        "pluginId": "cordova-plugin-media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-media/www/browser/Media.js",
+        "id": "cordova-plugin-media.BrowserMedia",
+        "pluginId": "cordova-plugin-media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-network-information/www/network.js",
         "id": "cordova-plugin-network-information.network",
         "pluginId": "cordova-plugin-network-information",
@@ -640,6 +568,40 @@ module.exports = [
         "id": "cordova-plugin-splashscreen.SplashScreenProxy",
         "pluginId": "cordova-plugin-splashscreen",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar.Browser",
+        "pluginId": "cordova-plugin-statusbar",
+        "merges": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/src/browser/Vibration.js",
+        "id": "cordova-plugin-vibration.Vibration",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -648,24 +610,24 @@ module.exports.metadata =
     "cordova-plugin-battery-status": "1.1.2",
     "cordova-plugin-camera": "2.1.1",
     "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-file": "4.3.2",
+    "cordova-plugin-file": "4.3.3",
     "cordova-plugin-media-capture": "1.2.0",
-    "cordova-plugin-console": "1.0.6",
+    "cordova-plugin-console": "1.0.7",
     "cordova-plugin-contacts": "2.0.1",
-    "cordova-plugin-device": "1.1.5",
-    "cordova-plugin-device-motion": "1.2.4",
-    "cordova-plugin-device-orientation": "1.0.6",
-    "cordova-plugin-media": "2.2.0",
-    "cordova-plugin-statusbar": "2.1.3",
-    "cordova-plugin-vibration": "2.1.4",
-    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-device-motion": "1.2.5",
+    "cordova-plugin-device-orientation": "1.0.7",
     "cordova-plugin-dialogs": "1.2.1",
     "cordova-plugin-file-transfer": "1.5.1",
     "cordova-plugin-geolocation": "2.1.0",
-    "cordova-plugin-globalization": "1.0.6",
+    "cordova-plugin-globalization": "1.0.7",
     "cordova-plugin-inappbrowser": "1.3.0",
+    "cordova-plugin-media": "2.2.0",
     "cordova-plugin-network-information": "1.2.1",
-    "cordova-plugin-splashscreen": "3.2.2"
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-vibration": "2.1.5",
+    "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -1,80 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
 
-function clr() {
-    // console.log("2");
-    document.getElementById("screen").value=0;
-        document.getElementById("screen").style="color:black;";
+// Cordova is ready
+//
+
+
+
+function onDeviceReady() {
+    console.log("Device Ready");
+    var db = window.openDatabase("shortName", "1.5", "displayName", 65536)
+    db.transaction(populateDB, errorCB, successCB);
 }
 
+function populateDB(tx) {
+     tx.executeSql('DROP TABLE IF EXISTS DEMO');
+     tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+     tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
 
-function inp(){
-    var show=document.getElementById("screen").value;
-    // console.log(show);
-    var len=show.length;
-    var first="";
-    var second="";
-    var oper="";
-    var flag=0;
-    var i=0;
-    if(show.charAt(i)=='-')
-    {
-        i=1;
-        first='-';
+function errorCB(err) {
+    alert("Error processing SQL: "+err.code);
+}
+
+function successCB() {
+    alert("success!");
+}
+
+function queryDB(tx) {
+    $("#name").val("working");
+    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+    // Query the success callback
+    //
+function querySuccess(tx, results) {
+  console.log("Returned rows = " + results.rows.length);
+  // this will be true since it was a select statement and so rowsAffected was 0
+  if (!results.rowsAffected) {
+    console.log('No rows affected!');
+    return false;
+  }
+  // for an insert statement, this property will return the ID of the last inserted row
+  console.log("Last inserted row ID = " + results.insertId);
     }
-    for(;i<len;i++)
-    {
-        if(show.charAt(i)!='+' && show.charAt(i)!='-' && show.charAt(i)!='*' && show.charAt(i)!='/' && show.charAt(i)!='=' && flag==0)
-        {
-             first=first+show.charAt(i);
-             // console.log(first);
-        }
-        else if(show.charAt(i)!='+' && show.charAt(i)!='-' && show.charAt(i)!='*' && show.charAt(i)!='/' && show.charAt(i)!='=' && flag==1)
-        {
-             second=second+show.charAt(i);
-             // console.log(second);
-        }
-        else if(flag==0){
-            flag=1;
-             oper=show.charAt(i);
-        }
-        else{
-            flag=2;
-            break;
-        }
+
+    // Transaction error callback
+    //
+    function errorCB(err) {
+        console.log("Error processing SQL: "+err.code);
     }
 
-    if(oper==='+')
-        show=parseFloat(first)+parseFloat(second);
-    else if(oper==='-')
-        show=parseFloat(first)-parseFloat(second);
-    else if(oper==='*')
-        show=parseFloat(first)*parseFloat(second);
-    else if(oper==='/')
-        show=parseFloat(first)/parseFloat(second);
-    // console.log(show);
-    first=parseFloat(show);
-    if(isNaN(show))
-    {
-        show="Syntax Error";
-        first="";
-        document.getElementById("screen").style="color:red;";
-    }
-    document.getElementById("screen").value=show;
+    // Transaction success callback
+    //
 
 
-}
-
-function post(ad){
-    var show=document.getElementById("screen").value;
-    show=show+ad;
-    document.getElementById("screen").value=show;
-}
-function back() {
-    var show=document.getElementById("screen").value;
-    show=show.substr(0,show.length-1);
-    document.getElementById("screen").value=show;
-}
-
-function change()
-{
-
-}
+$(".submit").click(function() {
+    var name = $("#name").val();
+        console.log("DB created");
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(queryDB, errorCB);
+})
